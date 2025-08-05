9c9e8289ba60ee7079fed8b9054f0f5609a08505
Michael Connor <necrydark@gmail.com>
2025-08-05 21:33:02 +0100

    updated colours

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-    <div className='w-full bg-[#072607] py-10 px-4'>
+    <div className='w-full border-t-[#072607] border-t-[1px] bg-black py-10 px-4'>
       <div className='container max-w-6xl mx-auto flex justify-between'>
         <p className="text-white">&copy; The Hunt 2025</p>
         <p className="text-white">We are not affiliated with Gearbox or 2K.</p>
