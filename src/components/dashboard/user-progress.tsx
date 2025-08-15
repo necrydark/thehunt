36476829d97c0c4430e3acc750e2b45519654d76
Michael Connor <necrydark@gmail.com>
2025-08-15 23:03:23 +0100

    updated env

--- src/components/dashboard/user-progress.tsx
+++ src/components/dashboard/user-progress.tsx
@@ -6,7 +6,7 @@ import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 interface Props {
