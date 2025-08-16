dd7eca2ca1efffc14acebd97db2098d5ec82008b
Michael Connor <necrydark@gmail.com>
2025-08-16 18:10:14 +0100

    updated login form colours

--- src/components/forms/login.tsx
+++ src/components/forms/login.tsx
@@ -1,37 +1,45 @@
-"use client"
-import { authClient } from '@/lib/auth-client'
-import { Twitch } from 'lucide-react'
-import { Button } from '../ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Twitch } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 
 export default function LoginForm() {
-
   const signInWithTwitch = async () => {
     await authClient.signIn.social({
       provider: "twitch",
-      callbackURL: "/dashboard"
-    })
-  }
+      callbackURL: "/dashboard",
+    });
+  };
 
   return (
     <div>
-        <Card>
-          <CardHeader>
-            <CardTitle>Login into your account.</CardTitle>
-            <CardDescription>Click the button below to login into your account.</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className='flex justify-center items-center'>
-                <Button
-                className='w-full bg-[#9146FF] hover:bg-[#9146FF]/75 duration-300 transition-all'
-                type="button"
-                onClick={signInWithTwitch}>
-                  <Twitch className='h-4 w-4 mr-2' />
-                  Login With Twitch
-                </Button>
-            </div>
-          </CardContent>
-        </Card>
+      <Card className="bg-black/20 border-primary-green/20 shadow-md shadow-primary-green/20 text-white">
+        <CardHeader>
+          <CardTitle>Login into your account.</CardTitle>
+          <CardDescription>
+            Click the button below to login into your account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex justify-center items-center">
+            <Button
+              className="w-full bg-[#9146FF] hover:bg-[#9146FF]/75 duration-300 cursor-pointer transition-all"
+              type="button"
+              onClick={signInWithTwitch}
+            >
+              <Twitch className="h-4 w-4 mr-2" />
+              Login With Twitch
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 }
